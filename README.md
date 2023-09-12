513efe1499b9af070280959d78bdb4ce9435c676
kinest22 <nirvana897@gmail.com>
2023-09-12 10:03:15 +0300

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Fiducuary-Calculator
+﻿# Fiduciary-Calculator
+
+Фидуциарный калькулятор - калькулятор для финансовых инструментов, отвечающий принципам фидуциарной ответственности, установленными Банком России.
+
+## Зависимости 
+- RuDataApi ([подробнее](https://github.com/Future-NPF-Investment-Department/RuData-API-for-CSharp))
---
+++ src/FiduCalc.cs
@@ -0,0 +1,15 @@
+﻿using RuDataAPI;
+using RuDataAPI.Extensions;
+
+namespace FiduciaryCalculator
+{
+    /// <summary>
+    ///     Fiduciary calculator. Provides static methods to find comparables to a bond, calculate yield, z-spread for a bond, etc.
+    /// </summary>
+    public static class FiduCal
+    {
+        private static EfirClient? _client;
+
+        // ...
+    }
+}
